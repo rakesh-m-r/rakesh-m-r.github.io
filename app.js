ca982d68b3add525d24c9bfcac41e3055a02047c
Rakesh M R <rakeshmr723@gmail.com>
2020-05-16 21:57:08 +0530

    mobile responsive

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+document.querySelector('.menu').style="display: none;"
 function menubar() {
     if (document.querySelector('.menubar').textContent == 'menu') {
       document.querySelector('.menubar').textContent = 'close'
+      document.querySelector('.menu').style="display: block;"
     } 
     else if(document.querySelector('.menubar').textContent == 'close') {
       document.querySelector('.menubar').textContent = 'menu'
+      document.querySelector('.menu').style="display: none;"
     }
   }
