4e1421aa765da7c042c60cea35cbaf78decb4c3a
Rakesh M R <rakeshmr723@gmail.com>
2020-05-20 18:45:32 +0530

    some changes

--- app.js
+++ app.js
@@ -5,9 +5,15 @@ function menubar() {
     document.querySelector('.menubar').textContent = 'close'
     document.querySelector('.menubar').style = "color: #fefefe"
     document.querySelector('.menu').style = "display: block;"
+    document.querySelector('.menu').style = "animation: fadeInLeft;animation-duration: 0.5s;"
+    document.querySelector('#ani0').style = "animation: heartBeat;animation-duration: 2s;";
+    document.querySelector('#ani1').style = "animation: heartBeat;animation-duration: 2s;";
+    document.querySelector('#ani2').style = "animation: heartBeat;animation-duration: 2s;";
+    document.querySelector('#ani3').style = "animation: heartBeat;animation-duration: 2s;";
+
   } else if (document.querySelector('.menubar').textContent == 'close') {
+    document.querySelector('.menu').style = "display: none"
     document.querySelector('.menubar').textContent = 'menu'
     document.querySelector('.menubar').style = "color: black"
-    document.querySelector('.menu').style = "display: none;";
   }
 }
