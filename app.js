14c7742334d1d5183531a65056876e5ec5359129
rakesh-m-r <rakeshmr723@gmail.com>
2021-09-30 22:58:46 +0530

    added: changed the styles of menu bar

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-document.querySelector('.menu').style = "display: none;"
 
 function menubar() {
-  if (document.querySelector('.menubar').textContent == 'menu') {
-    document.querySelector('.menubar').textContent = 'close'
-    document.querySelector('.menubar').style = "color: #fefefe"
-    document.querySelector('.menu').style = "display: block;"
-    document.querySelector('.menu').style = "animation: fadeInLeft;animation-duration: 0.5s;"
-    document.querySelector('#ani0').style = "animation: fadeInLeft;animation-duration: 2s;";
-    document.querySelector('#ani1').style = "animation: fadeInLeft;animation-duration: 2s;";
-    document.querySelector('#ani2').style = "animation: fadeInLeft;animation-duration: 2s;";
-    document.querySelector('#ani3').style = "animation: fadeInLeft;animation-duration: 2s;";
+    document.querySelector('.content').style = "display: none;"
+}
 
-  } else if (document.querySelector('.menubar').textContent == 'close') {
-    document.querySelector('.menu').style = "display: none"
-    document.querySelector('.menubar').textContent = 'menu'
-    document.querySelector('.menubar').style = "color: black"
+function adjust() {
+  const wrapper = document.querySelector(".wrapper");
+  const top = wrapper.getBoundingClientRect().top;
+  const content = document.querySelector(".content");
+
+  document.querySelector('.content').style = "display: flex;"
+
+  if (top < content.getBoundingClientRect().height) {
+    content.classList.add("down");
+  } else {
+    content.classList.remove("down");
   }
 }
+
