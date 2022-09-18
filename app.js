49ef1fa7fb57fcc29f247232cafc16938de5b1fd
Rakesh M R <rakeshmr723@gmail.com>
2022-09-18 06:35:17 +0000

    add: new design

--- app.js
+++ app.js
@@ -1,18 +1,72 @@
 
-function menubar() {
-    // document.querySelector('.content').style = "display: initial;"
-}
+let github = document.querySelector('.github');
 
-function adjust() {
-  const wrapper = document.querySelector(".wrapper");
-  const top = wrapper.getBoundingClientRect().top;
-  const content = document.querySelector(".content");
+let githubAnimation = bodymovin.loadAnimation({
+  container: github,
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: "./github.json"
+});
 
+github.addEventListener('mouseenter', function() {
+  githubAnimation.play();
+});
 
-  if (top < content.getBoundingClientRect().height) {
-    content.classList.add("down");
-  } else {
-    content.classList.remove("down");
-  }
-}
+github.addEventListener('mouseleave', function() {
+  githubAnimation.pause();
+});
 
+let instagram = document.querySelector('.instagram');
+
+let instagramAnimation = bodymovin.loadAnimation({
+  container: instagram,
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: "./instagram.json"
+});
+
+instagram.addEventListener('mouseenter', function() {
+  instagramAnimation.play();
+});
+
+instagram.addEventListener('mouseleave', function() {
+  instagramAnimation.pause();
+});
+
+let linkedin = document.querySelector('.linkedin');
+
+let linkedinAnimation = bodymovin.loadAnimation({
+  container: linkedin,
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: "./linkedin.json"
+});
+
+linkedin.addEventListener('mouseenter', function() {
+  linkedinAnimation.play();
+});
+
+linkedin.addEventListener('mouseleave', function() {
+  linkedinAnimation.pause();
+});
+
+let twitter = document.querySelector('.twitter');
+
+let twitterAnimation = bodymovin.loadAnimation({
+  container: twitter,
+  renderer: 'svg',
+  loop: true,
+  autoplay: false,
+  path: "./twitter.json"
+});
+
+twitter.addEventListener('mouseenter', function() {
+  twitterAnimation.play();
+});
+
+twitter.addEventListener('mouseleave', function() {
+  twitterAnimation.pause();
+});
