43f80b9fc5e92f216f0f68880a8e7983df79f022
Rakesh M R <rakeshmr723@gmail.com>
2025-04-05 13:21:37 +0530

    some updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,55 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import 'remixicon/fonts/remixicon.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "remixicon/fonts/remixicon.css";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Rakesh Mariyaplar',
-  description: 'I am a software engineer based in Bangalore with 2.5 years of experience in front-end technologies. Currently, I am working as a Software Development Engineer 2 (SDE-2) at Hashedin by Deloitte.',
-}
+  title: "Rakesh Mariyaplar",
+  description:
+    "I am a software engineer based in Bangalore with 2.5 years of experience in front-end technologies. Currently, I am working as a Software Development Engineer 2 (SDE-2) at Hashedin by Deloitte.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <title>Rakesh M R | Portfolio</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+        <meta name="title" content-1="Rakesh M R | Portfolio" />
+        <link rel="shortcut icon" href="profile.png" />
+        <link rel="apple-touch-icon" href="profile.png" />
+        <link rel="canonical" href="https://rakesh-m-r.github.io" />
+        <meta name="author" content-1="Rakesh M R" />
+        <meta
+          name="description"
+          content-1="Looking for a challenging role in a reputable organization to utilize my technical and
+  management skills for the growth of the organization as well as to enhance my knowledge
+  about new and emerging trends in the IT sector."
+        />
+        <meta name="author" content-1="Rakesh M R" />
+        <meta
+          name="keywords"
+          content-1="Rakesh, rakesh,rakesh m r, rakesh github profile, rakesh mariyaplar, rakesh-m-r, Rakesh M R, rakesh-mariyaplar, Rakesh Mariyaplar,Mariyaplar,rakesh,rakesh mariyaplar"
+        />
+        <meta
+          name="google-site-verification"
+          content-1="U-MBZQOJOSd_ptXqbECEyffs9jQ_qQM4AZpp20htKhs"
+        />
+        <meta
+          name="google-site-verification"
+          content-1="dw-WKwT-bsIBzHUjQ3_Om-gdxCfpNdMJEZfKs8iriKE"
+        />
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/sections/Info/index.tsx
+++ src/app/sections/Info/index.tsx
@@ -1,32 +1,109 @@
-import styles from './info.module.css';
-import {Avatar} from "@radix-ui/themes";
+"use client";
+import styles from "./info.module.css";
+import { Avatar } from "@radix-ui/themes";
 import Icon from "../../../components/Icon";
-import React,{ReactElement} from "react";
+import lottie from "lottie-web";
+import React, { ReactElement, useEffect } from "react";
 
-const InfoSection:React.FC<{}> = ():ReactElement =>{
-    return <><div id='about' className={styles.backgroud}>
-        <div className={styles.column}>
-            <svg aria-hidden="true" className={`${styles.hFull} ${styles.wFull}`}><defs><pattern id=":S2:" width="128" height="128" patternUnits="userSpaceOnUse" x="100%" y="100%" patternTransform="translate(112 64)"><path d="M0 128V.5H128" fill="none" stroke="currentColor"></path></pattern></defs><rect width="100%" height="100%" fill="url(#:S2:)"></rect></svg>
-        </div>
-        <div className={styles.column}>
-            <div className={styles.card}>
-                <div className={styles.imgWrapper}>
-                <Avatar className={styles.img} radius={"full"} size="9" src="/assets/profile.jpg" fallback={"RM"}/>
-                </div>
-                <h1 className={styles.heading}>Hey there, </h1>
-                <h1 className={styles.heading}>I&apos;m <span className={styles.highlight}>Rakesh Mariyaplar</span> </h1>
-                <p className={styles.description}>I am a software engineer based in Bangalore with 2+ years of experience in front-end technologies. Currently, I am working as a Software Development Engineer 2 (SDE-2) at Hashedin by Deloitte.</p>
-                <div className={styles.socialWrapper}>
-                    <a className={styles.link} target='_blank' href='https://github.com/rakesh-m-r'><Icon iconName={"ri-github-fill"}/></a>
-                    <a className={styles.link} target='_blank' href='https://www.linkedin.com/in/rakesh-m-r'><Icon iconName={"ri-linkedin-fill"}/></a>
-                    <a className={styles.link} target='_blank' href='https://twitter.com/rakesh_m_r_'><Icon iconName={"ri-twitter-x-fill"}/></a>
-                </div>
+const InfoSection: React.FC<{}> = (): ReactElement => {
+  useEffect(() => {
+    const elements = [
+      { selector: ".github", path: "/assets/github.json" },
+      { selector: ".linkedin", path: "/assets/linkedin.json" },
+      { selector: ".twitter", path: "/assets/twitter.json" },
+    ];
+
+    const animations = elements
+      .map(({ selector, path }) => {
+        const element = document.querySelector(selector);
+        if (!element) return null;
+
+        const animation = lottie.loadAnimation({
+          container: element,
+          renderer: "svg",
+          loop: false,
+          autoplay: false,
+          path: path,
+        });
+
+        const playAnimation = () => animation.play();
+        const stopAnimation = () => animation.goToAndStop(0, true);
+
+        element.addEventListener("mouseenter", playAnimation);
+        element.addEventListener("mouseleave", stopAnimation);
+
+        return { element, playAnimation, stopAnimation, animation };
+      })
+      .filter(Boolean);
+
+    return () => {
+      animations.forEach(
+        ({ element, playAnimation, stopAnimation, animation }) => {
+          element.removeEventListener("mouseenter", playAnimation);
+          element.removeEventListener("mouseleave", stopAnimation);
+          animation.destroy();
+        }
+      );
+    };
+  }, []);
+
+  return (
+    <>
+            <div className={styles.maxWidth}>
+
+      <div id="about" className={styles.background}>
+          <div className={styles.headingWrapper}>
+            <div>
+              <h1 className={styles.heading}>Hey there, </h1>
+              <h1 className={styles.heading}>
+                I&apos;m{" "}
+                <span className={styles.highlight}>Rakesh Mariyaplar</span>{" "}
+              </h1>
             </div>
+            <div className={styles.imgWrapper}>
+              <Avatar
+                className={styles.img}
+                radius={"full"}
+                size="9"
+                src="/assets/profile2.jpeg"
+                fallback={"RM"}
+              />
+            </div>
+          </div>
+          <p className={styles.description}>
+            I am a software engineer based in Bangalore with 4 years of
+            experience in Frontend and Backend technologies. Currently, I am
+            working as a Software Engineer II at SurveyMonkey.
+          </p>
+          <div className={styles.socialWrapper}>
+            <a
+              href="https://github.com/rakesh-m-r"
+              className={styles.socialWrap}
+            >
+              <div className="github"></div>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/rakesh-m-r"
+              className={styles.socialWrap}
+            >
+              <div className="linkedin"></div>
+            </a>
+            <a
+              href="https://twitter.com/rakesh_m_r_"
+              className={styles.socialWrap}
+            >
+              <div className="twitter"></div>
+            </a>
+          </div>
         </div>
-    </div>
-    <div className={styles.btnWrapper}><a className={styles.btn} href={"#experience"}><Icon iconName="ri-mouse-line"/></a></div>
-
+      </div>
+      <div className={styles.btnWrapper}>
+        <a className={styles.btn} href={"#experience"}>
+          <Icon iconName="ri-mouse-line" />
+        </a>
+      </div>
     </>
-}
+  );
+};
 
-export default  InfoSection;
+export default InfoSection;
